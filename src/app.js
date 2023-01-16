86219e7a8712d3f98aa379d1df2f257b35e9d4b0
Sergio k <valodya.kirik@gmail.com>
2023-01-16 20:23:43 +0200

    project has been added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const copyingFile = (file, copyFile) => {
+  if (file === copyFile) {
+    return;
+  }
+
+  const pathOne = path.join(__dirname, file);
+  const pathTwo = path.join(__dirname, copyFile);
+
+  if (!fs.existsSync(pathOne)) {
+    throw new Error('File does not exist');
+  }
+
+  try {
+    const dataFile = fs.readFileSync(pathOne, 'utf-8');
+
+    fs.writeFileSync(pathTwo, dataFile);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+copyingFile('file.txt', 'copyFile.txt');
